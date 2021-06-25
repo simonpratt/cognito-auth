da5a9579e359fe5d1bb09db830d163a7786d4c9f
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2021-06-25 20:39:53 +0930

    Added verificatin page export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export { default as AuthContext } from './context/Auth.context';
+export { default as CognitoAuthProvider } from './provider/CognitoAuth.provider';
+
 export { default as LoginPage } from './pages/Login.page';
 export { default as RegisterPage } from './pages/Register.page';
-export { default as CognitoAuthProvider } from './provider/CognitoAuth.provider';
+export { default as VerificationPage } from './pages/Verification.page';
 
 export { default as GuardedRoute } from './components/GuardedRoute.component';
 export { default as UnguardedRoute } from './components/UnguardedRoute.component';
