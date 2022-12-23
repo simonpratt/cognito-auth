51f9135781d9f84609cb65ad497e46aa0dca43c5
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-12-23 21:24:12 +1030

    Flexible peer deps and environment var warning for demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,9 +1,26 @@
+import { Alert, PaddedLayout } from '@dtdot/lego';
 import React from 'react';
 import AppAuthProvider from '../core/AppAuthProvider';
 import RootProvider from '../core/RootProvider';
 import Router from './Router';
 
 const App = () => {
+  const userPool = (import.meta as any).env.VITE_AWS_COGNITO_USER_POOL_ID;
+  const clientId = (import.meta as any).env.VITE_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID;
+
+  if (!userPool || !clientId) {
+    return (
+      <RootProvider>
+        <PaddedLayout>
+          <Alert
+            message='Missing environment! Please check the ".env.example" file and populate a ".env" file with the corresponding values'
+            variant='warn'
+          />
+        </PaddedLayout>
+      </RootProvider>
+    );
+  }
+
   return (
     <>
       <RootProvider>
